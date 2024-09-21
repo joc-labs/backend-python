ad413ef3bd44ccfd3d9c05db1d539635b473ca6e
iamthegoatai <goatoftradingai@gmail.com>
2024-09-21 12:13:35 +0200

    test workflow

--- app/main.py
+++ app/main.py
@@ -9,9 +9,8 @@ def start_app():
     ''' Initialises and configures application 
         including routes and api settings.
     '''
-
     app.include_router(user_routes)
-    print("App initialised!")
+    print("App initialised!!")
 
 
 start_app()
